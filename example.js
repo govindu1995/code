b65e7ba0c04497a2f78757546303b5a83b662e61
govinduchaitanyakrishna <govinduchaitanya3@gmail.com>
2022-08-09 22:00:20 +0530

    code

--- example.js
+++ example.js
@@ -8,6 +8,6 @@
 // console.log(obj);
 
 
-const fruits = ['a','z','c','d','e']; // alining an array
-const arr =fruits.sort();// sort with alining an array
-console.log(arr); //declering an array output in console.log()
+const fruits = ['a','z','c','d','e'];  // alining an array
+const arr =fruits.sort();   // sort with alining an array
+console.log(arr);     //declering an array output in console.log()
