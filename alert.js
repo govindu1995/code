e0aa67f0f5c079904d15edd9c1060305c974f5b2
govinduchaitanyakrishna <govinduchaitanya3@gmail.com>
2022-09-02 17:53:04 +0530

    code

--- alert.js
+++ alert.js
@@ -6,6 +6,6 @@ const r1 = readline.createInterface({
 });
 r1.question("what is your name ",function(answer){
     console.log(`oh so your name is ${answer}`);
-    console.log("Closing the interface");
+    // console.log("Closing the interface");
     r1.close();
 })
