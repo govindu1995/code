075dc2da9d34ace13ce2e24ab7b263ee4026868f
govinduchaitanyakrishna <govinduchaitanya3@gmail.com>
2022-09-05 16:15:01 +0530

    code

--- Quicksort.js
+++ Quicksort.js
@@ -34,3 +34,45 @@ var arr = [8,4,3,6,2,8,1,7];
 
 division(arr, 0, arr.length-1);
 console.log(arr);
+
+
+// // division 
+// function division(arr, left, right, k) {
+//     if(left >= right) return;
+                                     
+//     var P = placement(arr, left, right);
+
+//     // left sub array 
+//     division(arr, left, P-1)
+
+//     // right sub array 
+//     division(arr, P+1, right)
+// }
+
+// // placement of pivot 
+// function placement(arr, left, right) {
+//     var i=left, j=left, pivot = right;
+
+//     for(i=left; i<right; i++) {
+//         if(arr[i] <= arr[pivot]) {
+//             swalgo(arr, i, j);
+//             j++;
+//         }
+//     }
+//     // final / pivot swap
+//     swalgo(arr, j, pivot);
+//     return j;
+// }
+
+// // swapping algorithm
+// function swalgo(arr, i, j) {
+//     if(arr[i] === arr[j]) return;
+    
+//     var temp = arr[i];
+//     arr[i] = arr[j];
+//     arr[j] = temp;
+// }
+
+// var arr = [7,3,8,10,5,4,2,9];
+// division(arr, 0, arr.length-1, 2);
+// console.log(arr);
